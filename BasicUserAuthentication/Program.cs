66d431f83b510a1ae10b4cff86f22c5bf240acaf
mbukovich <markbukovich46@gmail.com>
2022-05-18 19:39:34 -0500

    Add project files.

---
+++ BasicUserAuthentication/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Identity.Client;
+
+namespace BasicUserAuthentication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var clientID = "cc327a90-2c2d-4be2-b294-c044f49adadb";
+            var redirectURI = "https://login.microsoftonline.com/common/oauth2/nativeclient";
+            string[] scopes = { "User.Read", "OnlineMeetings.ReadWrite" };
+
+            var client = PublicClientApplicationBuilder.Create(clientID)
+                .WithAuthority(AadAuthorityAudience.AzureAdAndPersonalMicrosoftAccount)
+                .WithRedirectUri(redirectURI)
+                .Build();
+
+            var token = client.AcquireTokenInteractive(scopes).ExecuteAsync().Result;
+            Console.WriteLine(token);
+        }
+    }
+}
